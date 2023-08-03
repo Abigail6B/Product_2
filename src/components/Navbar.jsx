b6e446356485dbd4eaa3744a02b469fe886f289f
Abigail6B <utp0145723@alumno.utpuebla.edu.mx>
2023-08-02 21:30:29 -0600

    Actualizacion navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import DAF from '../assets/DAF.png'
 
 export const Navbar =() => {
@@ -21,16 +22,21 @@ export const Navbar =() => {
 
         <ul className="navbar-nav">
           <li className="nav-item">
-              <a className="nav-link" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars" style={blanco}></i></a>
+              <a className="nav-link text-white" data-widget="pushmenu" href="#" role="button"><i className="fas fa-bars"></i></a>
           </li>
         </ul>
 
         <ul className="navbar-nav ml-auto">
         
         <li className="nav-item">
-            <a className="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button" >
-            <i className="fas fa-sign-out-alt" style={blanco}></i>
+            <Link to="/" className="brand-link">
+            <a className="nav-link text-white" data-widget="control-sidebar" data-slide="true" href="" role="button" >
+            
+            <i className="fas fa-sign-out-alt"></i>
+           
             </a>
+            </Link>
+            
         </li>
         </ul>
     </nav>
